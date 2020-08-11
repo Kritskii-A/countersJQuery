fea10006974c9f3cf3fbdd30a5b02ba8f2818397
Kritskii-A <62431514+Kritskii-A@users.noreply.github.com>
2020-08-11 17:14:43 +0300

    v. 0.1
    1. Increase counter value every second.
    2. Increase the counter by a random value after 5 seconds.
    3. Increase the counter by a random value after clicking on the button.

---
+++ counts.js
@@ -0,0 +1,47 @@
+// Increase counter value every second
+
+function counterFirst() {
+  let counter = $(".counter").html();
+  const interval = setInterval(function () {
+    counter++;
+    $(".counter").html(counter);
+  }, 1000); //timeout
+}
+
+counterFirst();
+
+// End
+
+// Increase the counter by a random value after 5 seconds.
+
+function counterRandom() {
+  let counterRand = $(".counter-random").html();
+  const interval = setInterval(function () {
+    counterRand = +counterRand + randomInteger();
+    $(".counter-random").html(counterRand);
+  }, 5000); //timeout
+}
+
+counterRandom();
+
+// End
+
+// Increase the counter by a random value after clicking on the button
+
+function counterClick() {
+  let counterClicking = $(".counter-click").html();
+  counterClicking = +counterClicking + randomInteger();
+  $(".counter-click").html(counterClicking);
+}
+// END
+
+$("button").click(function () {
+  counterClick();
+});
+
+// Random
+function randomInteger(min = 5, max = 30) {
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
+// END Random
